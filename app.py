90f5064c4d9aba62fe26ecef9a13c894de20f80a
Choihyunsu <chs991209@gmail.com>
2021-11-29 15:29:01 +0900

    app에 db 생성 추가

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask
 
 from api_v1 import api as api_v1
+from models import db
 
 app = Flask(__name__)
 app.register_blueprint(api_v1, url_prefix='/api/v1')
@@ -15,6 +16,9 @@ app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'dalskdfjlskfjlskjflaksjflk'
 
+db.init_app(app)
+db.app = app
+db.create_all()
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-class Todo(db.model):
+class Todo(db.Model):
     __tablename__ = 'todo'
 
     id = db.Column(db.Integer, primary_key=True)
