33f005e9a4b9a32cd13778d8e8a6e6cc6608f878
Choihyunsu <chs991209@gmail.com>
2021-11-30 17:22:32 +0900

    Fcuser Model 생성, Todo와 relationship으로 연결

--- models.py
+++ models.py
@@ -7,6 +7,7 @@ class Todo(db.Model):
     __tablename__ = 'todo'
 
     id = db.Column(db.Integer, primary_key=True)
+    fcuser_id = db.Column(db.Integer, db.ForeignKey('fcuser.id'), nullable=False)
     title = db.Column(db.String(256))
     tstamp = db.Column(db.DateTime, server_default=db.func.now())
 
@@ -17,3 +18,11 @@ class Todo(db.Model):
             'title': self.title,
             'tstamp': self.tstamp
         }
+
+
+class Fcuser(db.Model):
+    __tablename__ = 'fcuser'
+    id = db.Column(db.Integer, primary_key=True)
+    userid = db.Column(db.String(32))
+    password = db.Column(db.String(128))
+    todos = db.relationship('Todo', backref='fcuser', lazy=True)
