27ab6288c4e2d3916aa5b68512fb1fc16184c1f5
Choihyunsu <chs991209@gmail.com>
2021-11-30 16:19:51 +0900

    flasktodo create list res_msg 전송

--- api_v1/todo.py
+++ api_v1/todo.py
@@ -1,8 +1,17 @@
+import datetime
+
 import requests
 from flask import jsonify
 from flask import request
 
+from models import Todo, db
 from . import api
+
+
+def send_slack(msg):
+    res = requests.post('https://hooks.slack.com/services/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', json={
+        'text': msg
+    }, headers={'Content-Type': 'application/json'})
 
 
 @api.route('/todos', methods=['GET', 'POST'])
@@ -18,8 +27,30 @@ def todos():
     return jsonify(data)
 
 
-@api.route('/test', methods=['POST'])
-def test():
-    res = request.form['text']
-    print(res)
-    return jsonify(res)
+@api.route('/slack/todos', methods=['POST'])
+def slack_todos():
+    res = request.form['text'].split(' ')
+    cmd, *args = res
+
+    global ret_msg
+    ret_msg = ''
+
+    if cmd == 'create':
+        todo_name = args[0]
+
+        todo = Todo()
+        todo.title = todo_name
+
+        db.session.add(todo)
+        db.session.commit()
+
+        ret_msg = 'Todo Generated!'
+        send_slack('[%s] "%s" 할 일을 만들었습니다' % (str(datetime.datetime.now()), todo_name))
+
+    elif cmd == 'list':
+        todos = Todo.query.all()
+        for idx, todo in enumerate(todos):
+            # ret_msg += "" + str(idx+1) + " " + todo.title + " " + str(todo.tstamp)
+            ret_msg += '%d %s (~ %s)\n' % (idx+1, todo.title, str(todo.tstamp))
+
+    return ret_msg
